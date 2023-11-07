4af710c1225cb869c3c8ff1ddb34528d9b569183
toch <tocha.mateusz+main@gmail.com>
2023-11-07 17:42:29 +0100

    feat: add ssl, and post model serialization

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,45 @@
-use std::sync::Mutex;
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use serde::{Deserialize, Serialize};
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#[derive(Serialize, Debug, Deserialize)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World")
+async fn index(_req: HttpRequest) -> impl Responder {
+    let point = Point { x: 1, y: 2 };
+    serde_json::to_string(&point).unwrap()
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-struct AppStateWithCounter {
-    counter: Mutex<i32>,
-}
-
-#[get("/index")]
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
-
-    format!("Request number: {counter}")
+#[post("/test")]
+async fn check(info: web::Json<Point>) -> impl Responder {
+    println!("{:?}", info);
+    web::Json(Point {
+        x: info.x + 2,
+        y: info.y + 3,
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(counter.clone())
-            .service(hello)
-            .service(echo)
-            .service(index)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    match builder.set_private_key_file("key.pem", SslFiletype::PEM) {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    match builder.set_certificate_chain_file("cert.pem") {
+        Ok(_) => (),
+        Err(error) => panic!("Chain problem error occure:{error}"),
+    }
+
+    HttpServer::new(|| App::new().service(index).service(check))
+        .bind_openssl("127.0.0.1:8080", builder)?
+        .shutdown_timeout(10)
+        .run()
+        .await
 }
